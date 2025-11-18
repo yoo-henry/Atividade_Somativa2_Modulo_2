0160d04cbe426cc6181a915a679d9f2c60f61776
Henrique da Cruz Lima <cruzhenrique263@gmail.com>
2025-11-18 14:22:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,17 +8,34 @@ def index():
     return render_template("index.html")
 
 #Primeira Rota é soma
-
-
+@app.route("/soma")
+def soma():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 + v2}
 # Segunda é subtrair 
-
+@app.route("/subtrair")
+def subtrair():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 - v2}
 
 # Terceira é multiplicar
-
+@app.route("/multiplicar")
+def multiplicar():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado": v1 * v2}
 
 
 # Terceira é dividir 
-
+@app.route("/dividir")
+def dividir():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    if v2 == 0:
+        return {"erro": "Divisão por zero não é permitida"}
+    return {"resultado": v1 / v2}
 
 # 🚨🚨🚨🚨🚨🚨🚨🚨🚨 Não mexa aqui, pois isso que executa o arquivo 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
 if __name__ == "__main__":
